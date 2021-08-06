410b0df2904b75aa2abd2313d87466295649aa06
Brian Kidney <bkidney@briankidney.ca>
2021-08-06 13:45:10 -0230

    Adds missing gettime.h

---
+++ gettime.h
@@ -0,0 +1,10 @@
+#if defined(__x86_64__)
+static __inline__ unsigned long long rdtsc(void)
+{
+  unsigned hi, lo;
+  __asm__ __volatile__ ("rdtsc" : "=a"(lo), "=d"(hi));
+  return ( (unsigned long long)lo)|( ((unsigned long long)hi)<<32 );
+}
+#endif 
+
+
